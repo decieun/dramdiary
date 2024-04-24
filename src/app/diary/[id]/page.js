7bceeede87ea55b7aea8a746064f16c83bffcf42
decieun <166519882+decieun@users.noreply.github.com>
2024-04-24 21:59:02 +0900

    Update page.js

--- src/app/diary/[id]/page.js
+++ src/app/diary/[id]/page.js
@@ -1,5 +1,5 @@
 export default async function List(props) {
-  const resp = await fetch(`http://localhost:9090/items/${props.params.id}`);
+  const resp = await fetch(`https://dramdiary-json-server.vercel.app/items/${props.params.id}`);
   console.log(resp)
   const item = await resp.json();
   return (
